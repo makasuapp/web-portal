225ded14f39e3bdcad855ef0a856bdad57a72499
Owen Wang <wangfowen@gmail.com>
2020-10-09 11:12:59 -0400

    UnitField can create arbitrary unit

--- src/app/components/common/form/UnitField.tsx
+++ src/app/components/common/form/UnitField.tsx
@@ -1,16 +1,39 @@
 import UnitConverter, { allUnits } from 'app/utils/UnitConverter'
 import React from 'react'
-import { SelectField } from './SelectField'
-import { WrappedFieldProps } from './WrappedField'
+import CreatableSelect from 'react-select/creatable'
+import { WrappedField, WrappedFieldProps } from './WrappedField'
+
+import { Field } from 'formik'
 
 export function UnitField(props: WrappedFieldProps) {
+  const { name, label, customclasses = {} } = props
+
+  const options = [{ value: '', label: '' }].concat(
+    UnitConverter.unitOptions(allUnits)
+  )
+
   return (
-    <SelectField
-      label="Unit"
-      {...props}
-      options={[{ value: '', label: '' }].concat(
-        UnitConverter.unitOptions(allUnits)
+    <Field name={name}>
+      {({ field, form, meta }) => (
+        <WrappedField {...props} meta={meta} label={label || 'Unit'}>
+          <CreatableSelect
+            isClearable
+            name={field.name}
+            options={options}
+            value={
+              options.find((option) => option.value === field.value) || {
+                label: field.value,
+                value: field.value,
+              }
+            }
+            onChange={(option) =>
+              form.setFieldValue(field.name, (option as any).value)
+            }
+            onBlur={field.onBlur}
+            className={customclasses.input}
+          />
+        </WrappedField>
       )}
-    />
+    </Field>
   )
 }
